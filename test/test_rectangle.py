f77465a3b2442753e5beb0fc981e281a015622cd
LaQerty <72326588+LaQerty@users.noreply.github.com>
2023-07-23 14:48:56 +0300

    Update test_rectangle.py

--- test/test_rectangle.py
+++ test/test_rectangle.py
@@ -5,7 +5,8 @@ import pytest
 
 def test_init_rectangle():
     r = Rectangle(12, 12)
-    assert True
+    assert r.a == 12
+    assert r.b == 12
 
 
 def test_area_rectangle():
@@ -17,7 +18,8 @@ def test_change_area_rectangle():
     r = Rectangle(4, 5)
     f_area = r.area
     r.a = 5
-    assert r.area != f_area and r.area == 5 * 5
+    assert r.area != f_area 
+    assert r.area == 5 * 5
 
 
 def test_perimeter_rectangle():
@@ -29,7 +31,8 @@ def test_change_perimeter_rectangle():
     r = Rectangle(4, 5)
     f_perimeter = r.perimeter
     r.a = 5
-    assert r.perimeter != f_perimeter and r.perimeter == 2 * 5 + 2 * 5
+    assert r.perimeter != f_perimeter 
+    assert r.perimeter == 2 * 5 + 2 * 5
 
 
 def test_add_area_rectangle():
