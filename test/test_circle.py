7bd2de4b7e0346ae3836a0eb604997ec496d08e9
LaQerty <72326588+LaQerty@users.noreply.github.com>
2023-07-23 14:43:08 +0300

    Update test_circle.py

--- test/test_circle.py
+++ test/test_circle.py
@@ -7,7 +7,7 @@ PI = 3.14
 
 def test_init_circle():
     c1 = Circle(12)
-    assert True
+    assert c1.r == 12
 
 
 def test_area_circle():
@@ -19,7 +19,8 @@ def test_change_area_circle():
     c1 = Circle(4)
     f_area = c1.area
     c1.r = 5
-    assert c1.area != f_area and c1.area == PI * 5 * 5
+    assert c1.area != f_area 
+    assert c1.area == PI * 5 * 5
 
 
 def test_perimeter_circle():
@@ -31,7 +32,8 @@ def test_change_perimeter_circle():
     c1 = Circle(4)
     f_perimeter = c1.perimeter
     c1.r = 5
-    assert c1.perimeter != f_perimeter and c1.perimeter == 2 * 5 * PI
+    assert c1.perimeter != f_perimeter 
+    assert c1.perimeter == 2 * 5 * PI
 
 
 def test_add_area_circle():
